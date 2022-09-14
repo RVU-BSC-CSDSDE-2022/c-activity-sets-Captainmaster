8d8e6c6c66de886a1ddc1ef33983597218a2e827
Gokul   S <99109551+Captainmaster@users.noreply.github.com>
2022-09-14 14:33:26 +0000

    m

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,4 +1,4 @@
-[3:17 PM, 9/14/2022] Janvi RVU: #include <stdio.h>
+#include <stdio.h>
 int input()
 {
   int a;
@@ -12,36 +12,6 @@ int compare(int a, int b, int c)
     return a;
   }
   else if(a<=b &&b>=c){
-     return b;
-  }
-  else{
-    return c;
-  }
-}
-int main()
-{
-  int x,y,z,largest;
-  x=input();
-  y=input();
-  z=input();
-  largest=compare(x,y,z);
-  printf("the largest number of %d, %d and %d is %d",x,y,z,largest);
-  return 0;
-}
-[3:25 PM, 9/14/2022] Janvi RVU: #include <stdio.h>
-int input()
-{
-  int a;
-  printf("enter number\n");
-  scanf("%d",&a);
-  return a;
-}
-int compare(int a, int b, int c)
-{
-  if(a>=b && a>=c){
-    return a;
-  }
-  else if(a<=b && b>=c){
      return b;
   }
   else{
